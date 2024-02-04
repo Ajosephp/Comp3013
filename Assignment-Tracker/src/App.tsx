4888a9309a67ff380baa428d83a0c0302c52741b
Ajosephp <Ajosephpeterson@gmail.com>
2024-02-04 13:06:51 -0800

    [NEW] Handling form submission (to console.log) and preventing page refresh.

--- Assignment-Tracker/src/App.tsx
+++ Assignment-Tracker/src/App.tsx
@@ -1,7 +1,10 @@
 import { Header } from "./components/Header";
 import { Assignments } from "./components/Assignments";
+import { useState } from "react";
 
 function App() {
+
+  // const [assignments, setAssignments] = useState([]);
 
   return (
     <>
--- Assignment-Tracker/src/components/Header/index.tsx
+++ Assignment-Tracker/src/components/Header/index.tsx
@@ -3,18 +3,30 @@ import { AiOutlinePlusCircle } from "react-icons/ai";
 import { uppercase } from "../../helpers/stringHelpers";
 import { useState } from "react";
 
-
 export function Header() {
   const [input, setInput] = useState(''); // Default for the Input
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+    console.log("User entered: ", input)
+    setInput(''); // Set back to blank input
+  }
   
   return (
     <header className={styles.header}>
       {/* This is simply to show you how to use helper functions */}
       <h1>{uppercase("bcit")} Assignment Tracker</h1>
-      <form className={styles.newAssignmentForm}>
-        <input value={input} onChange={e => setInput(e.target.value)}
+      <form
+      onSubmit={handleSubmit} // Handle the form submission
+      className={styles.newAssignmentForm}>
+
+        <input 
+         value={input}
+         onChange={e => setInput(e.target.value)}
          type="text" />
-        <button disabled={!input}>
+        <button 
+         type="submit"
+         disabled={!input}>
           Create <AiOutlinePlusCircle size={20}/>
         </button>
       </form>
